5466d0e65e1386364d0ebf8b86010c7e902862b9
Victor Municelli Dário <vmunidario@gmail.com>
2021-06-07 16:01:06 -0300

    Update health.ts

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,11 +1,15 @@
 import { FastifyInstance } from 'fastify';
+import {
+  HealthSchema,
+  validatorCompiler,
+} from './schema';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
   fastify
-    .get('/health/alive', async function () {
+    .get('/health/alive', { schema: HealthSchema, validatorCompiler }, async function () {
       return { message: 'ok' };
     })
-    .get('/health/ready', async function () {
+    .get('/health/ready', { schema: HealthSchema, validatorCompiler }, async function () {
       return { message: 'ok' };
     });
 }
